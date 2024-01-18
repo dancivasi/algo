d4eb14a2c0202f329a4789f8c351875a2af92a08
dancivasi <vasilicavasilica26@yahoo.com>
2024-01-18 19:42:09 +0200

    changed the code to hidecreditcardinfo

--- hidecreditcardinfo.py
+++ hidecreditcardinfo.py
@@ -1,5 +1,16 @@
-card_number = input("Write card numbers: ")
-card_number_hidden = ""
-for i in card_number[:-4]:
-    card_number_hidden += "*"
-print(card_number_hidden + card_number[-4:])
+def hide_a_card_number():
+    card_number = input("Write card numbers: ")
+    card_number_hidden = ""
+    for i in card_number[:-4]:
+        card_number_hidden += "*"
+    print(card_number_hidden + card_number[-4:])
+
+def ask_for_card_change():
+    changecard = input("do you want to change the card number ? [yes] or [no]")
+    if changecard == "yes":
+        hide_a_card_number()
+    else:
+        return
+
+hide_a_card_number()
+ask_for_card_change()
